1487e678195ee914699a46033eeba9c135df3288
Omar Salah <omar.salah1597@gmail.com>
2025-12-24 18:07:25 +0200

    Add the design only (no logic added) for 'Add a sample credential' icon button, apply new desing on the text inoputs, and add new font verdana (using Arimo for now)
    Signed-off-by: Omar Salah <itsdevomar@gmail.com>

--- app/styles/theme/themes/darkTheme.ts
+++ app/styles/theme/themes/darkTheme.ts
@@ -18,7 +18,7 @@ export const darkTheme: ThemeType = {
     iconActive: Color.TitleGreen,
     iconInactive: Color.Gray400,
     inputInactive: Color.Gray400,
-    buttonPrimary: Color.TitleGreen,
+    buttonPrimary: Color.BrightGreen,
     buttonSecondary: Color.Gray600,
     buttonDisabled: Color.Gray500,
     shadow: Color.Black,
@@ -37,7 +37,8 @@ export const darkTheme: ThemeType = {
     regular: FontFamily.GT_AmericaRegular,
     medium: FontFamily.GT_AmericaMedium,
     bold: FontFamily.GT_AmericaBold,
-    mono: FontFamily.RobotoMonoRegular
+    mono: FontFamily.RobotoMonoRegular,
+    verdana: FontFamily.Verdana
   },
   fontSize: {
     xl: 48,
@@ -47,7 +48,7 @@ export const darkTheme: ThemeType = {
     regular: 16,
     small: 12
   },
-  borderRadius: 5,
+  borderRadius: 0,
   iconSize: 24,
   issuerIconSize: 48,
   statusBarStyle: ColorScheme.Light,
