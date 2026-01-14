f9480e4cb03f492fea5c3216b407530ec91ecdb5
Omar Salah <omar.salah1597@gmail.com>
2026-01-14 12:32:49 +0200

    update display name to 'DCW', change launch screen text, rename logo asset

--- app.config.js
+++ app.config.js
@@ -55,12 +55,14 @@ export const LinkConfig = {
  * @see https://docs.expo.dev/versions/latest/config/app/
  */
 export default {
-  displayName: 'T3 Digital Credential Wallet',
+  displayName: 'DCW',
+  launchScreenText:
+    'Save for continuing education while you earn, store, and share your workplace and skill credentials.',
   expo: {
     systemUIAppearance: 'automatic',
     runtimeVersion: VERSION_NUMBER,
     version: VERSION_NUMBER,
-    name: 'T3 Digital Credential Wallet',
+    name: 'DCW',
     slug: 'learner-credential-wallet',
     scheme: 't3-digital-credential-wallet',
     orientation: 'portrait',
