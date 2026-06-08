85b6d627624cc7d0d19d4a8b5ac0f98c68611a2e
Dmitri Zagidulin <dzagidulin@gmail.com>
2026-06-08 00:01:27 -0400

    Remove react-native-base64 and rn-animated-ellipsis deps.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- app/types/import.ts
+++
@@ -1,4 +0,0 @@
-declare module 'react-native-base64' {
-  export function decode(input: string): string
-  export function encode(input: string): string
-}
--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,6 @@ const packagesToTransformWithBabel = [
   '@testing-library/react-native',
   '@expo/vector-icons',
   'immer',
-  'rn-animated-ellipsis',
   'react-native-outside-press',
   '@noble/hashes' // ESM-only (type: module); used by app/lib/cid.ts
 ]
